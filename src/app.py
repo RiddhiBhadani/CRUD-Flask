d95fa5ee81751bc18b54dc47758b08c8b9f19863
Riddhi Patel <riddhibhadani7@gmail.com>
2025-06-30 22:27:59 -0400

    updated app.py and added .env to configure database connection

--- src/app.py
+++ src/app.py
@@ -3,6 +3,9 @@ from flask import Flask, request, jsonify
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
+from dotenv import load_dotenv
+load_dotenv()
+
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 app = Flask(__name__)
